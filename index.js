88a7842ce3fb293e8fc07790a2ef05b08da6e13f
emre.ozhan <emreozhan71@gmail.com>
2019-11-21 00:17:26 +0300

    index html add

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ var util = require('./Util');
 var HashMap = require('hashmap');
 var repository = require('./repository');
 
+
+
+
+var http = require('http');
+var fileSystem = require('fs');
+
+var server = http.createServer(function(req, resp){
+	fileSystem.readFile('./index.html', function(error, fileContent){
+		if(error){
+			resp.writeHead(500, {'Content-Type': 'text/plain'});
+			resp.end('Error');
+		}
+		else{
+			resp.writeHead(200, {'Content-Type': 'text/html'});
+			resp.write(fileContent);
+			resp.end();
+		}
+	});
+});
+
+server.listen(8080);
+
+/* 
+
 console.log('KickOff' + new Date() + '-');
 repository.createDb();
 
@@ -18,4 +42,4 @@ setInterval( function x(params) {
     
         HB.getPrice(products.HepsiBurada, counter);
     return x;
-}(), 24000);//5min
+}(), 24000);//5min */
